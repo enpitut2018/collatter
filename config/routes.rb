dc7d76e95aee22c7ef8e99bcfedd4ea03fe02e02
Qiushi Pan <qiu.gits@gmail.com>
2018-10-26 15:47:03 +0900

    Fix: wrong routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
-  get 'collas/show'=>'collas#show'
-
   get 'static_pages/dummy_edit_template'
   get 'static_pages/dummy_edit_colla'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
