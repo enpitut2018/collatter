000caf16a63351ab1277877281555dd91d8c57a1
kohei0309 <k.nishikawa0309@gmail.com>
2018-11-14 14:56:33 +0900

    carriererwaveについていじりました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181026072450) do
+ActiveRecord::Schema.define(version: 20181114051204) do
 
   create_table "collas", force: :cascade do |t|
     t.string "image"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20181026072450) do
   create_table "templates", force: :cascade do |t|
     t.string "image"
     t.string "tag_txt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
