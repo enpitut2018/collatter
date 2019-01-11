feb8cb59e7516dfc99ce19db691e4b186e7d9dad
Qiushi Pan <qiu.gits@gmail.com>
2019-01-11 16:32:52 +0900

    temporally. ajax wip

--- app/assets/javascripts/colla.js
+++ app/assets/javascripts/colla.js
@@ -1,44 +1,43 @@
 // turbolinks:load でロード時に読み込む．
 // https://qiita.com/hiroyayamamo/items/b258acbaa089d9482c8a
 $(document).on('turbolinks:load', function() {
-  $(function(){
-    // id="colla_image"の変化でコールバック
-    $("#colla_image").change(function(){
-      // 選択ファイルの有無をチェック
-      if (!this.files.length) {
-        alert('ファイルが選択されていません');
-        return;
-      }
-      // Formからファイルを取得
-      var file = this.files[0];
-      // (1) HTMLのCanvas要素の取得
-      var canvas = $("#cnvs");
-      // (2) getContext()メソッドで描画機能を有効にする
-      var ctx = canvas[0].getContext('2d');
-      // 描画イメージインスタンス化
-      var image = new Image();
-      // File API FileReader Objectでローカルファイルにアクセス
-      var fr = new FileReader();
-      // 画像がロードされた後にcanvasに描画を行う [非同期処理]
-      image.onload = function() {
-        // (3) プレビュー(Cnavas)のサイズを指定
-        var cnvsH = 300;
-        var cnvsW = image.naturalWidth*cnvsH/image.naturalHeight;
-        // (4) Cnavasにサイズアトリビュートを設定する
-        canvas.attr('width', cnvsW);
-        canvas.attr('height', cnvsH);
-        // (5) 描画
-        ctx.drawImage(image, 0, 0, cnvsW, cnvsH);
-      }
-      // ファイル読み込み読み込み完了後に実行 [非同期処理]
-      fr.onload = function(evt) {
-        // 読み込んだ画像をimageのソースに設定
-        image.src = evt.target.result;
-      }
-      // fileを読み込む データはBase64エンコードされる
-      fr.readAsDataURL(file);
-    })
+  // id="colla_image"の変化でコールバック
+  $("#colla_image").change(function(){
+    // 選択ファイルの有無をチェック
+    if (!this.files.length) {
+      alert('ファイルが選択されていません');
+      return;
+    }
+    // Formからファイルを取得
+    var file = this.files[0];
+    // (1) HTMLのCanvas要素の取得
+    var canvas = $("#cnvs");
+    // (2) getContext()メソッドで描画機能を有効にする
+    var ctx = canvas[0].getContext('2d');
+    // 描画イメージインスタンス化
+    var image = new Image();
+    // File API FileReader Objectでローカルファイルにアクセス
+    var fr = new FileReader();
+    // 画像がロードされた後にcanvasに描画を行う [非同期処理]
+    image.onload = function() {
+      // (3) プレビュー(Cnavas)のサイズを指定
+      var cnvsH = 300;
+      var cnvsW = image.naturalWidth*cnvsH/image.naturalHeight;
+      // (4) Cnavasにサイズアトリビュートを設定する
+      canvas.attr('width', cnvsW);
+      canvas.attr('height', cnvsH);
+      // (5) 描画
+      ctx.drawImage(image, 0, 0, cnvsW, cnvsH);
+    }
+    // ファイル読み込み読み込み完了後に実行 [非同期処理]
+    fr.onload = function(evt) {
+      // 読み込んだ画像をimageのソースに設定
+      image.src = evt.target.result;
+    }
+    // fileを読み込む データはBase64エンコードされる
+    fr.readAsDataURL(file);
   })
+
   //canvas内での座標をクリックで取得して、テキストの表示位置に代入したい
   $(document).on('click', '#cnvs', function(event) {
     console.log('clicked canvas');
@@ -77,4 +76,32 @@ function OnButtonClick(){
   ctx.textBaseline = 'middle';
   ctx.fillStyle = '#002B69';
   ctx.fillText(colla_text, this.colla_x, this.colla_y);
+}
+
+function Upload(){
+  console.log('convert to file');
+  var dataURL=canvas[0].toDataURL();
+  var blobBin = atob(dataURL.split(',')[1]);
+  var array = [];
+  for(var i = 0; i < blobBin.length; i++) {
+      array.push(blobBin.charCodeAt(i));
+  }
+  var fileBlob=new Blob([new Uint8Array(array)], {type: 'image/png'});
+  var file = new File([fileBlob], "name");
+
+  var form = $('form#colla_new');
+  var formdata = new FormData();
+  formdata.append("colla_image", file);
+
+  $.ajax({
+      url: '/collas/new',
+      type: 'POST',
+      data: formdata,
+      processData: false,
+      contentType: false,
+      dataType: 'json'
+    })
+      .done(function(data){
+        console.log(data);
+      })
 }
