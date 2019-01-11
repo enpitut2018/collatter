b18e994c1e3e27d6ad1e7de86280d6c113b71306
Qiushi Pan <qiu.gits@gmail.com>
2019-01-11 14:35:33 +0900

    Refactor: fix onclick function not found

--- app/assets/javascripts/colla.js
+++ app/assets/javascripts/colla.js
@@ -19,19 +19,19 @@ $(document).on('turbolinks:load', function() {
       var image = new Image();
       // File API FileReader Objectでローカルファイルにアクセス
       var fr = new FileReader();
+      // 画像がロードされた後にcanvasに描画を行う [非同期処理]
+      image.onload = function() {
+        // (3) プレビュー(Cnavas)のサイズを指定
+        var cnvsH = 300;
+        var cnvsW = image.naturalWidth*cnvsH/image.naturalHeight;
+        // (4) Cnavasにサイズアトリビュートを設定する
+        canvas.attr('width', cnvsW);
+        canvas.attr('height', cnvsH);
+        // (5) 描画
+        ctx.drawImage(image, 0, 0, cnvsW, cnvsH);
+      }
       // ファイル読み込み読み込み完了後に実行 [非同期処理]
       fr.onload = function(evt) {
-        // 画像がロードされた後にcanvasに描画を行う [非同期処理]
-        image.onload = function() {
-          // (3) プレビュー(Cnavas)のサイズを指定
-          var cnvsH = 300;
-          var cnvsW = image.naturalWidth*cnvsH/image.naturalHeight;
-          // (4) Cnavasにサイズアトリビュートを設定する
-          canvas.attr('width', cnvsW);
-          canvas.attr('height', cnvsH);
-          // (5) 描画
-          ctx.drawImage(image, 0, 0, cnvsW, cnvsH);
-        }
         // 読み込んだ画像をimageのソースに設定
         image.src = evt.target.result;
       }
@@ -52,28 +52,29 @@ $(document).on('turbolinks:load', function() {
     // 要素内におけるクリック位置を計算
     colla_x = clickX - positionX ;
     colla_y = clickY - positionY ;
-    document.getElementById("address").innerText = "x = " + colla_x + ", y = " + colla_y;
+    document.getElementById("colla-text").innerText = "x = " + colla_x + ", y = " + colla_y;
     //var canvas = $("#cnvs");
     //var ctx = canvas[0].getContext('2d');
     //ctx.fillStyle = '#000000';
     //ctx.clearRect(colla_x, colla_y, 30, 30);
   });
-  function OnButtonClick(){
-    // (1) HTMLのCanvas要素の取得
-    var canvas = $("#cnvs");
-    // テキストボックスの文字を取得する
-    var colla_text = $('#colla-text').val();
-    //var colla_x = colla_x.value;
-    //var colla_y = colla_y.value;
-    // (2) getContext()メソッドで描画機能を有効にする
-    var ctx = canvas[0].getContext('2d');
-
-    ctx.fillStyle = '#00000000';
-    ctx.fillRect(100, 100, 140, 30);
-    ctx.font = "bold 20px 'MS Pゴシック'";
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.fillStyle = '#002B69';
-    ctx.fillText(colla_text, this.colla_x, this.colla_y);
-  }
 });
+
+function OnButtonClick(){
+  // (1) HTMLのCanvas要素の取得
+  var canvas = $("#cnvs");
+  // テキストボックスの文字を取得する
+  var colla_text = $('#colla-text').val();
+  //var colla_x = colla_x.value;
+  //var colla_y = colla_y.value;
+  // (2) getContext()メソッドで描画機能を有効にする
+  var ctx = canvas[0].getContext('2d');
+
+  ctx.fillStyle = '#00000000';
+  ctx.fillRect(100, 100, 140, 30);
+  ctx.font = "bold 20px 'MS Pゴシック'";
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  ctx.fillStyle = '#002B69';
+  ctx.fillText(colla_text, this.colla_x, this.colla_y);
+}
