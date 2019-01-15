d52be267af4f8e2deea3399c284361823117076e
Qiushi Pan <qiu.gits@gmail.com>
2019-01-16 06:55:42 +0900

    functional conde. refactor to remove unnecessary parts

--- app/assets/javascripts/colla.js
+++ app/assets/javascripts/colla.js
@@ -1,6 +1,6 @@
 // turbolinks:load でロード時に読み込む．
 // https://qiita.com/hiroyayamamo/items/b258acbaa089d9482c8a
-$(document).on('ready', function() {
+$(document).on('turbolinks:load', function() {
   // id="colla_image"の変化でコールバック
   $("#colla_image").change(function(){
     // 選択ファイルの有無をチェック
@@ -36,44 +36,10 @@ $(document).on('ready', function() {
     }
     // fileを読み込む データはBase64エンコードされる
     fr.readAsDataURL(file);
-
-
-    var canvas = $("#cnvs");
-    var ctx = canvas[0].getContext('2d');
-    var dataURL=canvas[0].toDataURL('image/jpeg');
-    var blobBin2 = dataURLToBlob(dataURL);
-    var tag_txt = 'dddummy'
-    var formdata = new FormData({
-      'colla': {
-        'image': blobBin2,
-        'tag_txt': tag_txt
-      }
-    });
-    console.log(formdata);
-    formdata.append('image', blobBin2);
-    $.ajax({
-        url: '/collas',
-        type: 'POST',
-        //data: formdata,
-        data: formdata,
-        // data: {
-        //   colla: {
-        //     image: file,
-        //     tag_txt: 'dummy'
-        //   }
-        // },
-        processData: false,
-        // contentType: false,
-        contentType: 'application/json',
-        dataType: 'json'
-      })
-        .done(function(data){
-          console.log(data);
-        });
   })
 
   //canvas内での座標をクリックで取得して、テキストの表示位置に代入したい
-  $(document).on('click', '#cnvs', function(event) {
+  $('#cnvs').on('click', function(event) {
     console.log('clicked canvas');
     var clickX = event.pageX;
     var clickY = event.pageY;
@@ -91,30 +57,6 @@ $(document).on('ready', function() {
     //ctx.fillStyle = '#000000';
     //ctx.clearRect(colla_x, colla_y, 30, 30);
   });
-
-  function dataURLToBlob(dataURL) {
-    var BASE64_MARKER = ';base64,';
-    if (dataURL.indexOf(BASE64_MARKER) == -1) {
-        var parts = dataURL.split(',');
-        var contentType = parts[0].split(':')[1];
-        var raw = parts[1];
-
-        return new Blob([raw], {type: contentType});
-    }
-
-    var parts = dataURL.split(BASE64_MARKER);
-    var contentType = parts[0].split(':')[1];
-    var raw = window.atob(parts[1]);
-    var rawLength = raw.length;
-
-    var uInt8Array = new Uint8Array(rawLength);
-
-    for (var i = 0; i < rawLength; ++i) {
-        uInt8Array[i] = raw.charCodeAt(i);
-    }
-
-    return new Blob([uInt8Array], {type: contentType});
-  }
 
   $('#colla_new').submit(function(){
     var canvas = $("#cnvs");
@@ -144,97 +86,4 @@ function OnButtonClick(){
   ctx.textBaseline = 'middle';
   ctx.fillStyle = '#002B69';
   ctx.fillText(colla_text, this.colla_x, this.colla_y);
-}
-
-function dataURLToBlob(dataURL) {
-    var BASE64_MARKER = ';base64,';
-    if (dataURL.indexOf(BASE64_MARKER) == -1) {
-        var parts = dataURL.split(',');
-        var contentType = parts[0].split(':')[1];
-        var raw = parts[1];
-
-        return new Blob([raw], {type: contentType});
-    }
-
-    var parts = dataURL.split(BASE64_MARKER);
-    var contentType = parts[0].split(':')[1];
-    var raw = window.atob(parts[1]);
-    var rawLength = raw.length;
-
-    var uInt8Array = new Uint8Array(rawLength);
-
-    for (var i = 0; i < rawLength; ++i) {
-        uInt8Array[i] = raw.charCodeAt(i);
-    }
-
-    return new Blob([uInt8Array], {type: contentType});
-}
-function Upload(){
-  // (1) HTMLのCanvas要素の取得
-  var canvas = $("#cnvs");
-  console.log(canvas);
-  // テキストボックスの文字を取得する
-  var colla_text = $('#colla-text').val();
-  //var colla_x = colla_x.value;
-  //var colla_y = colla_y.value;
-  // (2) getContext()メソッドで描画機能を有効にする
-  var ctx = canvas[0].getContext('2d');
-  console.log(ctx);
-
-  console.log('convert to file');
-  var dataURL=canvas[0].toDataURL('image/jpeg');
-  var blobBin = atob(dataURL.split(',')[1]);
-  var blobBin2 = dataURLToBlob(dataURL);
-  var array = [];
-  for(var i = 0; i < blobBin.length; i++) {
-      array.push(blobBin.charCodeAt(i));
-  }
-  var fileBlob=new Blob([new Uint8Array(array)], {type: 'image/png'});
-  var file = new File([fileBlob], "name.png");
-
-  //var form = $('form#colla_new');
-  //var formdata = new FormData();
-  //formdata.append("image", fileBlob);
-  //formdata.append("colla[tag_txt]", $('#colla_tag_txt').val());
-  //formdata.append("tag_txt", 'dummy text');
-  var formdata = new FormData({
-    'colla': {
-      'image': blobBin2,
-      'tag_txt': tag_txt
-    }
-  });
-  var tag_txt = $('#colla_tag_txt').val();
-  var data = JSON.stringify({
-    'colla': {
-      'image': blobBin2,
-      'tag_txt': tag_txt
-    }
-  });
-
-  $.ajax({
-      url: '/collas',
-      type: 'POST',
-      //data: formdata,
-      data: formdata,
-      // data: {
-      //   colla: {
-      //     image: file,
-      //     tag_txt: 'dummy'
-      //   }
-      // },
-      processData: false,
-      // contentType: false,
-      contentType: 'application/json',
-      dataType: 'json'
-    })
-      .done(function(data){
-        console.log(data);
-      });
-}
-
-function GetDataURL() {
-  var canvas = $("#cnvs");
-  // hiddenフィールドにバイナリの中身をかく．
-  var dataurl = canvas[0].toDataURL();
-  $('#image_data_url').val(dataurl);
 }
