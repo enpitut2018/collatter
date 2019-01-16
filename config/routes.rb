a7f526db162e626ef7f0a79fa59529b144662d99
kohei0309 <k.nishikawa0309@gmail.com>
2019-01-16 14:18:10 +0900

    利用規約草案

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,6 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/about'
+  get 'static_pages/policy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
