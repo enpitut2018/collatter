09ab248f1476955579c3328ce36e4ab283c35073
Qiushi Pan <qiu.gits@gmail.com>
2019-01-16 22:27:16 +0900

    Able to logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,13 +4,6 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # remember user session for long term
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
   # return true if the passed user is currently logged in user
   def current_user?(user)
     user == current_user
@@ -19,12 +12,6 @@ module SessionsHelper
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(:remember, cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
     end
   end
 
@@ -32,15 +19,7 @@ module SessionsHelper
     !current_user.nil?
   end
 
-  # permanently abandon session
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
-  end
-
   def log_out
-    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
