bb4988e863695428e5f12e7736ab53e097d195e7
Qiushi Pan <qiu.gits@gmail.com>
2019-01-16 13:48:37 +0900

    template new with form in it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181214065448) do
+ActiveRecord::Schema.define(version: 20190116044031) do
 
   create_table "collas", force: :cascade do |t|
     t.string "image"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20181214065448) do
     t.string "tag_txt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "data"
   end
 
   create_table "users", force: :cascade do |t|
